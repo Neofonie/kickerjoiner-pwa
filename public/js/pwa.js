878126fee8cc73cfeabb5d3d7a9315320aa1d89e
Willy Woitas <willy.woitas@neofonie.de>
2021-10-21 23:23:43 +0200

    change scope

--- public/js/pwa.js
+++ public/js/pwa.js
@@ -81,7 +81,7 @@
     window.onload = () => {
         if ('serviceWorker' in navigator) {
             console.log(pre, 'init service worker')
-            const sw = navigator.serviceWorker.register('/js/service-worker.js?cb=' + window.cacheBuster, { scope: '/kickerjoiner-pwa/' })
+            const sw = navigator.serviceWorker.register('/kickerjoiner-pwa/js/service-worker.js?cb=' + window.cacheBuster)
                 .then((registration) => {
                     console.log(pre, 'service worker registered');
                 }).catch((error) => {
