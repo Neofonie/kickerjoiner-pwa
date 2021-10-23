26a49ecf37df88c486ef42540f902a1538e3c9d0
Willy Woitas <willy.woitas@neofonie.de>
2021-10-23 09:48:57 +0200

    test pwa renew #5

--- public/js/new-pwa.js
+++ public/js/new-pwa.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     if ('serviceWorker' in window.navigator) {
         window.navigator.serviceWorker
-            .register('/kickerjoiner-pwa/js/new-service-worker.js', { scope: '/kickerjoiner-pwa/js' });
+            .register('/kickerjoiner-pwa/js/new-service-worker.js', { scope: '/kickerjoiner-pwa/js/' });
     }
 }, { once: true });
